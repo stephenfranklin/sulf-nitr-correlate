b7ad540f1833d6c396c3c07134fb1d762bfef88d
Stephen Franklin <plamtruepub@gmail.com>
2014-04-16 16:29:59 -0500

    fixed for loop condition.

--- complete.R
+++ complete.R
@@ -22,15 +22,21 @@
 #     ## number of complete cases
 # }
 # ```
+rm(list = ls())            # clear the workspace
+
 
 complete <- function(directory, id = 1:332) {
     nobs <- vector(mode="numeric", length=length(id))
-    cc <- nobs
-    for(i in id){
+    for(i in 1:length(id)){
         file <- paste(getwd(),"/",directory,"/",sprintf("%03.f",
-                as.numeric(i)),".csv", sep="")
-        cc[i]<-sum(complete.cases(read.csv(file)))
+                as.numeric(id[i])),".csv", sep="")
+        cat(sprintf("i: %i,",i))
+        nobs[i]<-sum(complete.cases(read.csv(file)))
+        cat(sprintf("nobs: %i,",nobs[i]))
     } 
+    cat(sprintf("\n"))
+    cat(sprintf("nobs: %i\n",nobs))
     completes<-data.frame(id, nobs)
+    cat(sprintf("id: %i\n",id))
     return(completes)
 }
